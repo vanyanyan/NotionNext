028aef2a1f5092e8897eff740c5b31be44f8eb44
vanyanyan <31093488+vanyanyan@users.noreply.github.com>
2025-12-24 14:43:51 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -31,7 +31,7 @@ const CONFIG = {
   HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEXO_ARTICLE_COPYRIGHT: failse, // 显示文章版权声明
+  HEXO_ARTICLE_COPYRIGHT: false, // 显示文章版权声明
   HEXO_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
   HEXO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
