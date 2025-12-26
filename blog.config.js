51886aac060d567a0c33ebcdc4213d2ad8ecb01c
vanyanyan <nemo.y.lee@gmail.com>
2025-12-26 15:28:10 +0800

    modified:   blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'VPS雷达站旗下网站', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'VPS雷达站', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'VPS评测/推荐/使用', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://vpschain.nemoy.de', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'vps, vps评测,vps使用,vps购买,vps教程', // 网站关键词 英文逗号隔开
